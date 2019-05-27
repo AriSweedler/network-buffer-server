46ffb471186f6a94002ba72bcd507be556b6300e
Ari Sweedler <ari@sweedler.com>
2019-05-26 17:29:07 -0700

    Basic yank/put functionality working.
      * But you have to send GET reqs to the /yank route. We wanna do POST
      requests with potentially complex bodies.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = Number(process.env.PORT || 8000);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+let network_buffer = null;
+
+app.get('/', (req, res) => {
+  res.send(`Hello World! Your network buffer contains: '${network_buffer}'`);
+});
+
+app.get('/yank/:text', function (req, res) {
+  network_buffer = req.params.text;
+  res.send(`Got a YANK request: ${req.params.text}`);
+})
+
+app.get('/put', function (req, res) {
+  res.send(`${network_buffer}`);
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
